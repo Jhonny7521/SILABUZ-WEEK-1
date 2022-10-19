03d23036c4bed4958c5a994b9f7c2f487b835142
jjnietoc <juanjonietocastillo@gmail.com>
2022-10-18 21:53:40 -0500

    Ejercicios 1, 3 y 6

--- TODO.py
+++ TODO.py
@@ -2,6 +2,22 @@
 Ejercicio 1: Hacer un programa que pida al usuario 5 nombres. Crear una lista con los 5 nombres. Despues hacer que muestre una frase los pronombres que empiezan con la letra A
 """
 
+def letra_nom():
+    lista_nom = []
+
+    for i in range(5):
+        nombre = input("Agrega un nombre: ")
+        lista_nom.append(nombre)
+        print(lista_nom)
+
+    letra = input("Que letra deseas revisar? ")
+
+    result = [idx for idx in lista_nom if idx.lower().startswith(letra.lower())]
+
+    print(f"Los nombres que empiezan con la letra {letra}, son {result}")
+
+
+letra_nom()
 
 """
 Ejercicio 2: Haga un programa en Python que le pida al usuario tantos enteros como quiera, luego cree dos listas, una con la lista de números propuestos y la otra con
@@ -12,6 +28,14 @@ debe ser [4,8,9,7] y el segundo [3,1,1,2]
 """ *
 Ejercicio 3: Dada la matriz, [[1,2,3],[4,5,6],[7,8,9]], calcule el promedio de la diagonal principal. Hint: Los 3 elementos de la diagonal son 1,5,9
 """
+def sum_diag(dic):
+    suma_diag = sum(dic[i][i] for i in range(3))
+    print(str(suma_diag//3))
+
+
+m = [[1,2,3],[4,5,6],[7,8,9]]
+
+sum_diag(m)
 
 """ *
 Ejercicio 4: Dada la siguiente lista ["Hola", "Amigos", "Hoy", True] , escriba un programa que pida al usuario una palabra, dicha palabra debe ser agregada al final y al inicio de la lista.
@@ -50,6 +74,18 @@ Ingrese un nombre: Marcelo
 
 Salida: 1.80
 """
+def get_key(dict1, val):
+    for key, value in dict1.items():
+        if val == value:
+            return key
+
+    return "No existe este elemento"
+
+
+
+dict1 = {'Marcelo': 1.80, 'José':1.50, 'Oscar':1.70, 'Jorge': 1.40}
+print(get_key(dict1,(float(input("Que talla te interesa? ")))))
+
 
 """
 Ejercicio 7
